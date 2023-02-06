3b0eb556c6f2316fcfefd01f4a9f1a2a720d5377
bkdragon0228 <rlaqjarb2067@naver.com>
2023-02-07 01:57:22 +0900

    feat : dummy data 불러오기

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { fetcher, QueryKeys } from '../../queryClient';
 
 const ProductsPage = () => {
-  return <div>조회 페이지</div>;
+  const { data, isLoading } = useQuery(QueryKeys.PRODUCTS, () =>
+    fetcher({
+      method: 'GET',
+      path: '/products',
+    })
+  );
+
+  if (isLoading) {
+    return <div>Loading..</div>;
+  }
+
+  console.log(data);
+  return <div>상품 조회 페이지</div>;
 };
 
 export default ProductsPage;
--- src/queryClient.ts
+++ src/queryClient.ts
@@ -1,10 +1,6 @@
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query';
+import { QueryClient } from 'react-query';
+
+type AnyOBJ = { [key: string]: any };
 
 export const getClient = () => {
   let client: QueryClient | null = null;
@@ -14,4 +10,38 @@ export const getClient = () => {
   }
 
   return client;
+};
+
+const BASE_URL = 'https://fakestoreapi.com';
+
+export const fetcher = async ({
+  method,
+  path,
+  body,
+  params,
+}: {
+  method: 'GET' | 'POST' | 'DELETE' | 'PATCH';
+  path: string;
+  body?: AnyOBJ;
+  params?: AnyOBJ;
+}) => {
+  try {
+    const url = `${BASE_URL}${path}`;
+    const fetchOptions: RequestInit = {
+      method,
+      headers: {
+        'Content-type': 'application/json',
+        'Access-Control-Allow-Origin': BASE_URL,
+      },
+    };
+    const res = await fetch(url, fetchOptions);
+    const json = await res.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const QueryKeys = {
+  PRODUCTS: 'products',
 };
